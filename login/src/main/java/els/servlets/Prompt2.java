82a479845a600b9031518709fcd89e988c09447b
elsonscaria <elsonscaria77@gmail.com>
2022-08-03 00:38:28 -0500

    Day 11 Assignments - Servlet

--- login/src/main/java/els/servlets/Prompt2.java
+++ login/src/main/java/els/servlets/Prompt2.java
@@ -5,12 +5,13 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/Reg")
-public class Prompt2 {
+public class Prompt2 extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
